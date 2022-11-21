f9039d69580c93edf1134b6f87ae025664ed7de4
Soganyy <matinmammadli0420@gmail.com>
2022-11-21 14:00:40 +0400

    created page

--- src/App.js
+++ src/App.js
@@ -3,21 +3,28 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <span className="rectFirstBackground"></span>
+      <div>
+          <span className="rect1"></span>
+          <span className="rect2"></span>
+      </div>
+      <div className="hello"><p className="textHello">Hello;</p></div>
+      <div className="info"><p className="infoText">I’m Matin Mammadli.
+          I like making fun, interactive things with code.
+          I also try to design.</p>
+      </div>
+      <div className="worksHeading"><p className="worksText">Works</p></div>
+      <div className="worksSection">
+          <span className="wrapElementBackground"></span>
+          <span className="wrapElementBackground"></span>
+          <span className="wrapElementBackground"></span>
+          <span className="wrapElementBackground"></span>
+          <span className="wrapElementBackground"></span>
+          <span className="wrapElementBackground"></span>
+      </div>
+      <div className="blurOnWrappedElements"><p className="textOnBlur">I’ve just started working on projects but you can check my GitHub.</p></div>
+
     </div>
   );
 }
